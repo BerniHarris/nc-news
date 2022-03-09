1db4b76f0c6ed35e9f479731d5db08678ecec309
BerniHarris <harrisbernigmail.com>
2022-03-09 10:59:01 +0000

    amend to be more consistent with other components

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom'
+import { useParams, Link } from 'react-router-dom'
 import { getArticle } from "../utils/api";
 
-const Article = () => {
+export default function Article() {
     const { article_id } = useParams();
     const [article, SetArticle] = useState([]);
     const [isLoading, SetIsLoading] = useState(true);
@@ -16,10 +16,14 @@ const Article = () => {
     return (
         <div className="content">
             <main className="article">
-                <h2 className="title">{article.title}</h2>
-                <p className="body">{article.body}</p>
+                <h2 className="article-title">{article.title}</h2>
+                <p className="article-body">{article.body}</p>
+                <p className="article-card_author">Created by {article.author}</p>
+                <p className="article-votes">Votes {article.votes}</p>
+            <Link to={`/topic/${article.topic}`}>
+                <p className="article-topic" className={`article-${article.topic}`}>{article.topic}</p>
+            </Link>
             </main>
         </div>
     );
 };
-export default Article;
