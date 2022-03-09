c040e28cd44c24dbb127dbb1bb9e45273c586756
BerniHarris <harrisbernigmail.com>
2022-03-09 17:42:06 +0000

    add patchVotes api

--- src/utils/api.js
+++ src/utils/api.js
@@ -4,22 +4,31 @@ const api = axios.create({
     baseURL: 'https://nc-news-example-seminar-3-2.herokuapp.com/',
  })
 
+//  ------ list of all articles ------
 exports.getAllArticles = (topic) => {
   if (!topic) {
     return api.get("/api/articles").then((res) => {
       return res.data.articles;
     });
   }
+ //  ------ list of all articles by topic------
   else {
       return api.get(`/api/articles?topic=${topic}`).then((res) => {
         return res.data.articles;
       });
   }
 };
-
+//  ------ article by id------
 exports.getArticle = (article_id) => {
   return api.get(`/api/articles/${article_id}`).then((res) => {
     return res.data.article;
   });
 };
+//  ------ votes ------
+exports.patchVotes = (article_id, inc_votes) => {
+  return api.patch(`/api/articles/${article_id}`, inc_votes).then((res) => {
+    console.log(res.data.articles)
+    return res.data.articles;
+  })
+}
 
