d30854802a86d489f559cf531a675aca21ac66e5
BerniHarris <harrisberni@gmail.com>
2022-04-13 21:11:23 +0100

    app changes

--- src/App.js
+++ src/App.js
@@ -1,42 +1,39 @@
 // style sheet
-import './App.css'; 
+import "./App.css";
 // router to create links to pages
-import {BrowserRouter, Route,Routes} from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 //imported components
-import Header from './components/Header';
-import Footer from './components/Footer';
-import ArticleList from './components/ArticleList';
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import ArticleList from "./components/ArticleList";
 //imported pages
-import Home from './pages/Home';
-import Login from './pages/Login';
-import Menu from './pages/Menu';
-import Article from './components/Article'
-import CommentCard from './components/ArticleComments';
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Menu from "./pages/Menu";
+import Article from "./components/Article";
 
-
-  {/*-----------------------Page----------------------- */}
 function App() {
-    return (
-      <BrowserRouter>
-    <div className="App">
-  {/*-----------------------Top of page----------------------- */}
-    <Header />
-    {/* <Banner /> --- dynamic pictre depending on route - figure out how to do! */}
-  {/*-----------------------routes----------------------- */}
-      <div className="content">
-      <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/menu" element={<Menu />} />
-          <Route path='/topic/:topic' element={<ArticleList />}/>
-          <Route path='/articles/:article_id' element={<Article />}/>
-      </Routes>    
-      </div>
-  {/*-----------------------Bottom of page----------------------- */}
+  return (
+    <BrowserRouter>
+      <div className="App">
+        {/*-----------------------Top of page----------------------- */}
+        <Header />
+        {/* <Banner /> --- dynamic pictre depending on route - figure out how to do! */}
+        {/*-----------------------routes----------------------- */}
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/menu" element={<Menu />} />
+            <Route path="/topic/:topic" element={<ArticleList />} />
+            <Route path="/articles/:article_id" element={<Article />} />
+          </Routes>
+        </div>
+        {/*-----------------------Bottom of page----------------------- */}
 
-    <Footer />
-    </div>
+        <Footer />
+      </div>
     </BrowserRouter>
   );
 }
