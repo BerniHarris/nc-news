9c635c380005d8bd04023294ca9b09141b63b553
BerniHarris <harrisberni@gmail.com>
2022-04-15 15:29:34 +0100

    add delete article

--- src/components/CommentCard.jsx
+++ src/components/CommentCard.jsx
@@ -1,15 +1,24 @@
 import React from "react";
 import moment from "moment";
+import { deleteArticleComment, getCommentsByArticle } from "../utils/api";
 
-export default function CommentCard(props) {
-  let created = props.created_at;
+export default function CommentCard(comment) {
+  const handleClick = () => {
+    deleteArticleComment(comment.comment_id).then(() => {
+      getCommentsByArticle(comment.article_id).then((receivedData) => {
+        return comment.setComments(receivedData);
+      });
+    });
+  };
+  let created = comment.created_at;
   return (
     <div>
       <dl className="individual-comment-box">
-        <dt>{props.body}</dt>
-        <dt>Author {props.author}</dt>
-        <dt>Votes {props.votes}</dt>
+        <dt>{comment.body}</dt>
+        <dt>Author {comment.author}</dt>
+        <dt>Votes {comment.votes}</dt>
         <dt>At {moment(created).format("MMM Do YYYY")}</dt>
+        <button onClick={handleClick}>Delete Comment</button>
       </dl>
     </div>
   );
