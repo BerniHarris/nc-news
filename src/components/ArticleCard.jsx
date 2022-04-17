48faaabab5d8d30e29d92a9c44303098419123f9
BerniHarris <harrisberni@gmail.com>
2022-04-17 19:33:30 +0100

    add stling to article cards

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -4,16 +4,22 @@ import { Link } from "react-router-dom";
 export default function ArticleCard(article) {
   //article is props
   return (
-    <div className="card">
-      <Link to={`/topic/${article.topic}`}>
-        <p className={`article-${article.topic}`}>{article.topic}</p>
-      </Link>
-      <Link to={`/articles/${article.article_id}`} className="article-card">
-        <p className="card_title">{article.title}</p>
-      </Link>
-      <p className="card_author">Created by {article.author}</p>
-      <p className="card_votes">Votes {article.votes}</p>
-      <p className="card_votes">Comments {article.comment_count}</p>
+    <div className={`${article.topic}-card`}>
+      <div>
+        <Link to={`/topic/${article.topic}`}>
+          <p className={`article-${article.topic}`}>
+            {article.topic.toUpperCase()}
+          </p>
+        </Link>
+        <Link to={`/articles/${article.article_id}`} className="article-card">
+          <p className="card_title">{article.title.toUpperCase()}</p>
+        </Link>
+      </div>
+      <div className="bylikecomment">
+        <p className="card_author">By {article.author}</p>
+        <p className="card_votes">Likes {article.votes}</p>
+        <p className="card_comments">Comments {article.comment_count}</p>
+      </div>
     </div>
   );
 }
