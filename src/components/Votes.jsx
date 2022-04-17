5a259c4c3cfdcdaf58005b5694539df228189204
BerniHarris <harrisberni@gmail.com>
2022-04-17 21:07:02 +0100

    style single aticle card

--- src/components/Votes.jsx
+++ src/components/Votes.jsx
@@ -1,24 +1,30 @@
 import React, { useState } from "react";
-import {patchVotes} from '../utils/api'
+import { patchVotes } from "../utils/api";
 
-export default function Voting ({votes, article_id}) {
-    const [votechange, setVotechange] = useState(0);
+export default function Voting({ votes, article_id }) {
+  const [votechange, setVotechange] = useState(0);
 
-    const handleIncrement = (inc_votes) => {
-      setVotechange((currCount) => {
-        return currCount + inc_votes;
+  const handleIncrement = (inc_votes) => {
+    setVotechange((currCount) => {
+      return currCount + inc_votes;
     });
     patchVotes(article_id, inc_votes).catch(() => {
       setVotechange((currCount) => {
         return currCount - inc_votes;
+      });
     });
-  })
-};    
-        return (
-          <div className="voting">
-            <button disabled ={votechange > 0} onClick={() => handleIncrement(1)}>+</button>
-            <p>Votes {votes + votechange}</p>
-            <button disabled ={votechange < 0} onClick={() => handleIncrement(-1)}>-</button>
-          </div>
-        );
-      };
+  };
+  return (
+    <div className="voting">
+      <p>Likes {votes + votechange}</p>
+      <div>
+        <button disabled={votechange > 0} onClick={() => handleIncrement(1)}>
+          +
+        </button>
+        <button disabled={votechange < 0} onClick={() => handleIncrement(-1)}>
+          -
+        </button>
+      </div>
+    </div>
+  );
+}
