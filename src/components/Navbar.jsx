34186d6c0091d4fae3fe9ed8bc1ef71801c81ae9
BerniHarris <harrisberni@gmail.com>
2022-04-21 00:58:13 +0100

    switch login page to side menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,11 +8,14 @@ import { IconContext } from "react-icons";
 import logo from "../pics/logo.png";
 import profileicon from "../pics/profileicon.png";
 import { NavbarData } from "./NavbarData";
+import avatar from "../pics/profileicon.png";
 
 function Navbar() {
   const [sidebar, setSidebar] = useState(false);
+  const [login, setLogin] = useState(false);
 
   const showSidebar = () => setSidebar(!sidebar);
+  const showLogin = () => setLogin(!login);
 
   return (
     <>
@@ -26,11 +29,12 @@ function Navbar() {
               <img src={logo} className="logo" alt="logo" />
             </Link>
           </div>
-          <Link to="/Login">
+          <Link to="#" className="profile">
             <img
+              onClick={showLogin}
               src={profileicon}
               className="profileicon"
-              height={"35px"}
+              height={"40px"}
               alt="icon of a profile using a pigeon sillohette instead of person"
             />
           </Link>
@@ -52,6 +56,27 @@ function Navbar() {
                 </li>
               );
             })}
+          </ul>
+        </nav>
+        ;
+        <nav className={login ? "login-menu active" : "login-menu"}>
+          <ul className="login-items" onClick={showLogin}>
+            <li className="login-toggle">
+              <Link to="#" className="profile">
+                <AiIcons.AiOutlineClose />
+              </Link>
+            </li>
+            <div className="login-text">
+              <img
+                src={avatar}
+                height={100}
+                className="pigeonprofie"
+                alt="pigeons take on basic profile icons"
+              />
+              <p>You are logged in as:</p>
+              <b />
+              <p>HAPPYAMY2016</p>
+            </div>
           </ul>
         </nav>
       </IconContext.Provider>
