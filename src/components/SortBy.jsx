217f9ae21f75b437007361953adbae9f967f2c1d
BerniHarris <harrisberni@gmail.com>
2022-04-20 02:53:49 +0100

    reword sorts

--- src/components/SortBy.jsx
+++ src/components/SortBy.jsx
@@ -17,12 +17,12 @@ export const SortBy = ({ setSort, setOrder }) => {
           <option value="" disabled selected>
             Sort By...
           </option>
-          <option value="created_at asc">Oldest</option>
           <option value="created_at desc">Newest</option>
-          <option value="votes asc">Least Votes</option>
-          <option value="votes desc">Most Votes</option>
-          <option value="comment_count asc">Least comments</option>
-          <option value="comment_count desc">Most comments</option>
+          <option value="created_at asc">Oldest</option>
+          <option value="votes desc">Most liked</option>
+          <option value="votes asc">Least liked</option>
+          <option value="comment_count desc">Most talked about</option>
+          <option value="comment_count asc">Least talked about</option>
           {/* //add comment sort */}
         </select>
       </label>
