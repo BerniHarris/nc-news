e59c50d41fc748aa16faa8a87accca0ec5a9fc34
BerniHarris <harrisberni@gmail.com>
2022-04-20 20:08:08 +0100

    change home to birdhouse icon

--- src/components/NavbarData.jsx
+++ src/components/NavbarData.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import * as FaIcons from "react-icons/fa";
-import * as AiIcons from "react-icons/ai";
+import Birdhouse from "./birdhouse";
 
 export const NavbarData = [
   {
     title: "Home",
     path: "/",
-    icon: <AiIcons.AiFillHome />,
+    icon: <Birdhouse />,
     cName: "nav-text",
   },
   {
