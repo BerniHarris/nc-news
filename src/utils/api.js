265c896ad51e22306e25647a38af753aac5b0768
BerniHarris <harrisberni@gmail.com>
2023-10-18 22:43:50 +0100

    chore - removed duplicate code and comments

--- src/utils/api.js
+++ src/utils/api.js
@@ -13,36 +13,23 @@ exports.getTopics = (topic, sort, order) => {
       return res.data.articles;
     });
 };
-//  ------ list of all articles ------
-exports.getAllArticles = (topic, sort, order) => {
-  if (!topic) {
-    return api
-      .get("/api/articles", {
-        params: { topic: topic, sort_by: sort, order: order },
-      })
-      .then((res) => {
-        return res.data.articles;
-      });
-  }
 
-  //  ------ list of all articles by topic------
-  else {
-    return api
-      .get(`/api/articles?topic=${topic}`, {
-        params: { sort_by: sort, order: order },
-      })
-      .then((res) => {
-        return res.data.articles;
-      });
-  }
+exports.getAllArticles = (topic, sort, order) => {
+  return api
+    .get("/api/articles", {
+      params: { topic: topic, sort_by: sort, order: order },
+    })
+    .then((res) => {
+      return res.data.articles;
+    });
 };
-//  ------ article by id------
+
 exports.getArticle = (article_id) => {
   return api.get(`/api/articles/${article_id}`).then((res) => {
     return res.data.article;
   });
 };
-//  ------ votes ------
+
 exports.patchVotes = (article_id, votes) => {
   return api
     .patch(`/api/articles/${article_id}`, { inc_votes: votes })
@@ -50,13 +37,13 @@ exports.patchVotes = (article_id, votes) => {
       return res;
     });
 };
-// ------ article comments ------
+
 exports.getCommentsByArticle = (article_id) => {
   return api.get(`/api/articles/${article_id}/comments`, {}).then((res) => {
     return res.data.comments;
   });
 };
-// ------ post comments ------
+
 exports.postArticleComment = (article_id, comment) => {
   return api
     .post(`/api/articles/${article_id}/comments`, comment)
