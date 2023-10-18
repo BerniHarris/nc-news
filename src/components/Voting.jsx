b47aff5fcd4448fa03fb20d9941ae5c70cdaf8e3
BerniHarris <harrisberni@gmail.com>
2023-10-18 09:32:26 +0100

    fix - Improved readability of Vote component

---
+++ src/components/Voting.jsx
@@ -0,0 +1,20 @@
+import like from "../pics/like.svg";
+import dislike from "../pics/dislike_comment.svg";
+
+export default function Voting(props) {
+  const change = props.voteChange;
+
+  return (
+    <div className="voting">
+      <button disabled={change > 0} onClick={props.upVote}>
+        <img src={like} alt="heart" className="up" />
+      </button>
+
+      <div className="p-likes">{props.votes}</div>
+
+      <button disabled={change < 0} onClick={props.downVote}>
+        <img src={dislike} alt="broken heart" className="down" />
+      </button>
+    </div>
+  );
+}
