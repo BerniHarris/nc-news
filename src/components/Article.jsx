d0dba4da4f9161e4ee3b4f69b3076666d42ecbbb
BerniHarris <harrisberni@gmail.com>
2023-10-18 09:34:21 +0100

    chore - Set props and updated useEffect for article to use Vote component

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from "react";
 import { useParams, Link } from "react-router-dom";
-import { getArticle, getCommentsByArticle } from "../utils/api";
-import Votes from "./Votes";
+import { getArticle, getCommentsByArticle, patchVotes } from "../utils/api";
 import PostComment from "./PostComment";
 import CommentCard from "./CommentCard";
+import Voting from "./Voting";
 import moment from "moment";
 import comment from "../pics/comment.svg";
 import "./Article.css";
@@ -13,68 +13,91 @@ export default function Article() {
   const [article, setArticle] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [comments, setComments] = useState([]);
+  const [reload, setReload] = useState(true);
+  const [votes, setVotes] = useState(0);
+  const [voteChange, setVoteChange] = useState(0);
 
   useEffect(() => {
-    getArticle(article_id).then((article) => {
-      setArticle(article);
-      setIsLoading(false);
-    });
-  }, [article_id, comments]);
+    if (reload) {
+      Promise.all([
+        getArticle(article_id),
+        getCommentsByArticle(article_id),
+      ]).then(([dataArticle, dataComments]) => {
+        setVotes(dataArticle.votes);
+        setArticle(dataArticle);
+        setComments(dataComments);
+        setIsLoading(false);
+        setReload(false);
+      });
+    }
+  }, [article_id, reload]);
 
-  useEffect(() => {
-    getCommentsByArticle(article_id).then((comments) => {
-      setComments(comments);
-    });
-  }, [article_id]);
+  const upVote = () => {
+    patchVotes(article_id, 1);
+    setVotes(votes + 1);
+    setVoteChange(voteChange + 1);
+  };
 
-  let created = article.created_at;
+  const downVote = () => {
+    patchVotes(article_id, -1);
+    setVotes(votes - 1);
+    setVoteChange(voteChange - 1);
+  };
 
-  if (isLoading) return <h2>loading...</h2>;
   return (
-    <div>
-      <div className="article-box">
-        <section className="article">
-          <div className="card_topic">
-            <Link to={`/topic/${article.topic}`}>
-              <p className={`article-${article.topic}`}>
-                {article.topic.toUpperCase()}
-              </p>
-            </Link>
+    <>
+      {isLoading ? (
+        <h2>loading...</h2>
+      ) : (
+        <div>
+          <div className="article-box">
+            <section className="article">
+              <div className="card_topic">
+                <Link to={`/topic/${article.topic}`}>
+                  <p className={`article-${article.topic}`}>
+                    {article.topic.toUpperCase()}
+                  </p>
+                </Link>
+              </div>
+              <h2 className="card_title">{article.title.toUpperCase()}</h2>
+              <div className="timeandby">
+                <p className="card_author">By {article.author}</p>
+                <p className="article_time">
+                  {moment(article.created_at).format("MMM Do YYYY")}
+                </p>
+              </div>
+              <p className="article-body">{article.body}</p>
+              <div className="articlebylikecomment">
+                <p className="articleCommentCount">
+                  <img
+                    src={comment}
+                    className="card_comments"
+                    alt="speech bubble icon"
+                  />
+                  {article.comment_count}
+                </p>
+                <Voting
+                  className="vote"
+                  article_id={article_id}
+                  votes={votes}
+                  upVote={upVote}
+                  downVote={downVote}
+                  voteChange={voteChange}
+                />
+              </div>
+            </section>
           </div>
-          <h2 className="card_title">{article.title.toUpperCase()}</h2>
-          <div className="timeandby">
-            <p className="card_author">By {article.author}</p>
-            <p className="article_time">
-              {moment(created).format("MMM Do YYYY")}
-            </p>
+          <PostComment setComments={setComments} article_id={article_id} />
+          <div>
+            <div>
+              <h3>Comments:</h3>
+              {comments.map((comment) => {
+                return <CommentCard key={comment.comment_id} {...comment} />;
+              })}
+            </div>
           </div>
-          <p className="article-body">{article.body}</p>
-          <div className="articlebylikecomment">
-            <p className="articleCommentCount">
-              <img
-                src={comment}
-                className="card_comments"
-                alt="speech bubble icon"
-              />
-              {article.comment_count}
-            </p>
-            <Votes
-              className="vote"
-              article_id={article_id}
-              votes={article.votes}
-            />
-          </div>
-        </section>
-        <PostComment setComments={setComments} article_id={article_id} />
-      </div>
-      {/* -----comments------ */}
-
-      <h3>Comments:</h3>
-      <div>
-        {comments.map((comment) => {
-          return <CommentCard key={comment.comment_id} {...comment} />;
-        })}
-      </div>
-    </div>
+        </div>
+      )}
+    </>
   );
 }
