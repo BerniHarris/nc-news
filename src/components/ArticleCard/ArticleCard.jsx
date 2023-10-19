688d63af1a676843c9dee232d31014a36f2ceb06
BerniHarris <harrisberni@gmail.com>
2023-10-19 12:57:01 +0100

    feat: Swapped JS code to media queries. Destructured props

--- src/components/ArticleCard/ArticleCard.jsx
+++ src/components/ArticleCard/ArticleCard.jsx
@@ -5,49 +5,30 @@ import comment from "../../pics/comment.svg";
 import "./ArticleCard.css";
 
 export default function ArticleCard(props) {
-  // ------ added as wanted long cards on a big screen but to fit on small. Flex box alone wasn't looking consistent in sizes ------
-  let articleWidth = window.innerWidth;
-  if (articleWidth <= 400) {
-    articleWidth = 240;
-  } else if (articleWidth > 400) {
-    articleWidth = 500;
-  }
-
-  let articleHeight = window.innerWidth;
-  if (articleHeight <= 400) {
-    articleHeight = 300;
-  } else if (articleHeight > 400) {
-    articleHeight = 200;
-  }
-
-  const article = props.article;
+  const { article_id, topic, author, title, comment_count, votes } =
+    props.article;
 
   return (
-    <div
-      className={`${article.topic}-card`}
-      style={{ width: `${articleWidth}px`, height: `${articleHeight}px` }}
-    >
+    <div className={`${topic}-card `}>
       <div>
         <div className="card_topic">
-          <Link to={`/topic/${article.topic}`}>
-            <p className={`article-${article.topic}`}>
-              {article.topic.toUpperCase()}
-            </p>
+          <Link to={`/topic/${topic}`}>
+            <p className={`article-${topic}`}>{topic.toUpperCase()}</p>
           </Link>
         </div>
-        <Link to={`/articles/${article.article_id}`} className="article-card">
-          <p className="card_title">{article.title.toUpperCase()}</p>
+        <Link to={`/articles/${article_id}`} className="article-card">
+          <p className="card_title">{title.toUpperCase()}</p>
         </Link>
       </div>
       <div className="bylikecomment">
-        <p className="card_author">By {article.author}</p>
+        <p className="card_author">By {author}</p>
         <p>
           <img src={like} className="card_votes" alt="vote icon" />
-          {article.votes}
+          {votes}
         </p>
         <p className="card_comments">
           <img src={comment} className="card_comments" alt="comment icon" />
-          {article.comment_count}
+          {comment_count}
         </p>
       </div>
     </div>
