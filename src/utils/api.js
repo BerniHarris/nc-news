e387f539a31013ca57de85f7f408eb6604bc7010
BerniHarris <harrisberni@gmail.com>
2024-03-10 21:34:41 +0000

    new url added

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 const axios = require("axios").default;
 
 const api = axios.create({
-  baseURL: "https://berni-nc-news-api.onrender.com/",
+  baseURL: "https://be-nc-news-4vle.onrender.com",
 });
 
 exports.getTopics = (topic, sort, order) => {
